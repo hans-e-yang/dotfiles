9e57805c92f082c3615f8b0b7837253373046453
Hans <hanseyang@outlook.com>
2026-09-14 09:29:58 +0800

    fix grep string telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,13 +2,13 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>pf", "<cmd>Telescope find_files<cr>"},
-    { "<C-p>", "<cmd>Telescope git_files<cr>"}
+    { "<leader>pf", "<cmd>Telescope find_files<cr>" },
+    { "<C-p>", "<cmd>Telescope git_files<cr>" },
+    { "<leader>ps", function()
+        require('telescope.builtin').grep_string({
+          search = vim.fn.input("Grep > "),
+          use_regex = false,
+        })
+      end, desc = "Grep string" },
   },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ")})
-    end)
-  end
 }
